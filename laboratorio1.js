fc8e088a0659ba187e8f79308a22d3780c9db85f
leidysrios <leidysrios20@gmail.com>
2024-12-02 08:09:50 -0500

    laboratorio1

---
+++ laboratorio1.js
@@ -0,0 +1,33 @@
+//laoraorio1  parte 1
+let nombre="leidys";
+let edad=24;
+let esEstudiante=false;
+
+console.log("nombre: "+nombre);
+console.log("edad: "+edad);
+console.log("Es estudiante:"+esEstudiante);
+
+let a=15;
+let b=5;
+
+//laboratorio1 pate 2
+
+let suma=a+b;
+let resta=a-b;
+let multi=a*b;
+let divi=a/b;
+
+console.log("suma:"+suma);
+console.log("resta:"+resta);
+console.log("multiplicacion:"+multi);
+console.log("division:"+divi);
+console.log("a es igual b: "+(a==b));
+
+//laboratorio1 parte 3
+
+console.log(`laboratorio 1-jercicio3`);
+ let esMayorDeEdad =false;
+ let tienelicecia =true;
+ //  console.log(`es mayr de edad:  ${esMayorDeEdad}`)
+ 
+ esMayorDeEdad  && tienelicencia ? console.log(`la persona puede conducir`) : console.log(`la persona "NO" puede conducir `);
